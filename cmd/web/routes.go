6e83bcefe2ffb6156125b4cbe1e409aec865e572
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-12-23 21:04:31 +0530

    feat(templates): :sparkles: Add templates for dog and cat breeds and breeders

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,16 +4,20 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() http.Handler {
-	mux := NewCustomRouter()
+	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
 	mux.Use(middleware.Timeout(60 * time.Second))
 
 	mux.Get("/", app.ShowHome)
+
+	// NOTE:  A very important reusability feature of the chi router is the ability to define routes with URL parameters. You can render any page by passing the page name as a URL parameter
+	mux.Get("/{page}", app.ShowPage)
 
 	return mux
 }
